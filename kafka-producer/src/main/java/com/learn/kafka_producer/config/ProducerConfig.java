f7686a3c82849d0286f38871a4cbe18f95d330e2
Nagasree <khyathimnsree369@gmail.com>
2025-12-03 20:37:47 +0530

    producer Factory

--- kafka-producer/src/main/java/com/learn/kafka_producer/config/ProducerConfig.java
+++ kafka-producer/src/main/java/com/learn/kafka_producer/config/ProducerConfig.java
@@ -1,14 +1,38 @@
 package com.learn.kafka_producer.config;
 
+import com.learn.kafka_producer.model.Product;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.config.TopicConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.Map;
 
 @Configuration
 public class ProducerConfig {
 
+    public Map<String, Object> producerConfig(){
+        return Map.of(org.apache.kafka.clients.producer.ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5",
+                org.apache.kafka.clients.producer.ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG,
+                "true");
+    }
+
+    @Bean
+    ProducerFactory<String, Product> producerFactory(){
+        return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+    @Bean
+    KafkaTemplate<String, Product> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
+    }
+
     @Bean
     public NewTopic topic(){
-        return new NewTopic("product-created-topic", 3, (short)1);
+        return new NewTopic("product-created-topic", 3, (short)1)
+                .configs(Map.of(TopicConfig.MIN_IN_SYNC_REPLICAS_CONFIG, "3"));
     }
 }
