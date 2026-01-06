7f69ee35142583633bb240c2c0703a781f344961
Nagasree <khyathimnsree369@gmail.com>
2026-01-06 23:39:45 +0530

    Dead Letter Topic

--- EmailNotificationService/src/main/java/com/learn/EmailNotificationService/config/ConsumerContainerConfig.java
+++ EmailNotificationService/src/main/java/com/learn/EmailNotificationService/config/ConsumerContainerConfig.java
@@ -1,6 +1,7 @@
 package com.learn.EmailNotificationService.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.example.model.Product;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,11 +9,12 @@ import org.springframework.boot.logging.LogLevel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
+import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -30,20 +32,32 @@ public class ConsumerContainerConfig {
                 ErrorHandlingDeserializer.class);
         configs.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JacksonJsonDeserializer.class);
         configs.put(ConsumerConfig.GROUP_ID_CONFIG, "email-group");
-        configs.put(JacksonJsonDeserializer.TRUSTED_PACKAGES, "org.example.model");
+//        configs.put(JacksonJsonDeserializer.TRUSTED_PACKAGES, "org.example.model");
         return new DefaultKafkaConsumerFactory(configs);
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Product> testCustomFactory(){
+    public ConcurrentKafkaListenerContainerFactory<String, Product> testCustomFactory(KafkaTemplate<String, Product> kafkaTemplate){
         ConcurrentKafkaListenerContainerFactory<String, Product> container =
                 new ConcurrentKafkaListenerContainerFactory<>();
         container.setConsumerFactory(consumerFactory());
-        DefaultErrorHandler errorHandler = new DefaultErrorHandler((record, exception) -> {
-            log.error("{{}} occurred during deserialization of {} event", exception.getMessage(),
-                    record.topic());
-        });
+        DeadLetterPublishingRecoverer deadLetterPublishingRecoverer =
+                new DeadLetterPublishingRecoverer(kafkaTemplate);
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler(deadLetterPublishingRecoverer);
         container.setCommonErrorHandler(errorHandler);
         return container;
+    }
+
+    public ProducerFactory<String, Product> producerFactory(){
+        Map<String, Object> map = new HashMap<>();
+        map.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        map.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, JacksonJsonSerializer.class);
+        map.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JacksonJsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(map);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Product> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
     }
 }
